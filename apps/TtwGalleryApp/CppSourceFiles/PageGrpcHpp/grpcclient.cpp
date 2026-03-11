fa3459113b6973cf3c497dd7f5cdac7270a28dad
WangSiQi <1658339000.qq.com>
2026-03-11 17:27:06 +0800

    feat(app grpc) : grpcclient.hpp 增加 一元\流 RPC 调用返回 待验证

--- apps/TtwGalleryApp/CppSourceFiles/PageGrpcHpp/grpcclient.cpp
+++ apps/TtwGalleryApp/CppSourceFiles/PageGrpcHpp/grpcclient.cpp
@@ -1,3 +1,90 @@
 #include "grpcclient.h"
 
-GrpcClient::GrpcClient() {}
+GrpcClient::GrpcClient(QObject *parent) : QObject(parent) {}
+
+
+void GrpcClient::setGrpcClient(routeguide::RouteGuide::Client *client) {
+    m_grpcClient = client;
+    return ;
+}
+
+template<typename Reply>
+void GrpcClient::handleReply(std::unique_ptr<QGrpcCallReply> reply,
+                             std::function<void(const Reply &)> onSuccess) {
+
+    QObject::connect(reply.get(), &QGrpcCallReply::finished,
+                     [reply = std::move(reply), onSuccess](const QGrpcStatus &status)mutable{
+        if(!status.isOk()) {
+            qDebug() << "[ gRPC::handleReply ] error :" << status.message();
+            return ;
+        }
+
+        auto respOpt = reply->read<Reply>();
+        if(respOpt) {
+            onSuccess(*respOpt);
+        } else {
+            qDebug() << "[ gRPC::handleReply ]returned : empty reply";
+        }
+
+    });
+}
+
+template<typename Reply>
+void GrpcClient::handleStreamReply(std::unique_ptr<QGrpcServerStream> reply,
+                       std::function<void(const Reply&)> onMessage,
+                       std::function<void(const QGrpcStatus &)> onFinished) {
+
+    QObject::connect(reply.get(), &QGrpcServerStream::messageReceived,
+                     [s_reply = reply.get(), onMessage]() mutable{
+
+        while(auto msg = s_reply->read<Reply>()) {
+            onMessage(*msg);
+        }
+
+    });
+
+    QObject::connect(reply.get(), &QGrpcServerStream::finished,
+                     [reply = std::move(reply), onFinished](const QGrpcStatus &status){
+        onFinished(status);
+    });
+
+}
+
+
+
+
+
+void GrpcClient::fetchGreeting(const QString &name) {
+    if(!m_grpcClient) return ;
+
+    m_isLoading = true;
+    emit isLoadingChanged();
+
+    // 构造请求
+    routeguide::Request req;
+    req.setId_proto(1001);
+    req.setData(name.toUtf8());
+
+    handleReply<routeguide::Response>(m_grpcClient->sayHello(req), [](const routeguide::Response &resp){
+        qDebug() << resp.message();
+    });
+
+
+
+
+    auto s_reply = m_grpcClient->ListFeatures(req);
+
+    handleStreamReply<routeguide::Response>(
+        std::move(s_reply),
+        [](const routeguide::Response &resp){
+            qDebug() << "Feature:" << resp.message();
+        },
+        [](const QGrpcStatus &status){
+            if (!status.isOk())
+                qDebug() << "Stream error:" << status.message();
+            else
+                qDebug() << "Stream finished successfully";
+    });
+
+    return ;
+}
--- apps/TtwGalleryApp/CppSourceFiles/PageGrpcHpp/grpcclient.h
+++ apps/TtwGalleryApp/CppSourceFiles/PageGrpcHpp/grpcclient.h
@@ -3,22 +3,65 @@
 
 #include <QObject>
 #include <QQmlEngine>
-
+#include <QGrpcCallReply>
+#include <QDebug>
 
 #include "stream.qpb.h"
 #include "stream_client.grpc.qpb.h"
 
-class GrpcClient : public QGrpcClientBase
+class GrpcClient : public QObject
 {
     Q_OBJECT
     QML_ELEMENT
     QML_SINGLETON
+
+    Q_PROPERTY(QString responseText READ responseText NOTIFY responseTextChanged)
+    Q_PROPERTY(bool isLoading READ isLoading NOTIFY isLoadingChanged)
+
+signals:
+    void responseTextChanged();
+    void isLoadingChanged();
+
 public:
-    GrpcClient();
+    explicit GrpcClient(QObject *parent = nullptr);
+
+    // 注入gRPC 客户端实例
+    void setGrpcClient(routeguide::RouteGuide::Client *client);
+
+    template<typename Reply>
+    ///
+    /// \brief handleReply 一元 RPC 调用返回
+    /// \param reply 包含 gRPC 回复数据的智能指针。函数会接管该指针的所有权，在内部使用完毕后自动销毁。
+    /// \param onSuccess 可选的完成回调函数。当回复成功且数据有效时调用，无参数无返回值。
+    ///
+    void handleReply(std::unique_ptr<QGrpcCallReply> reply,
+                     std::function<void(const Reply&)> onSuccess);
+
+    ///
+    /// \brief handleStreamReply 服务器流 RPC 调用返回
+    /// \param reply
+    /// \param onMessage
+    /// \param onFinished
+    ///
+    template<typename Reply>
+    void handleStreamReply(std::unique_ptr< QGrpcServerStream > reply,
+                           std::function< void (const Reply &) > onMessage,
+                           std::function< void (const QGrpcStatus &) > onFinished);
+
+
+    Q_INVOKABLE void fetchGreeting(const QString &name);
+    QString responseText() const { return m_responseText; }
+    bool isLoading() const { return m_isLoading; }
 
 private:
-    std::unique_ptr<QAbstractGrpcChannel> m_channel;
-    routeguide::RouteGuideClient *m_client; // 命名规则：包名::服务名Client
+    QString m_responseText;
+    bool m_isLoading = false;
+
+
+    // 必须管理 Reply 对象的生命周期，否则请求会被意外取消
+    std::unique_ptr<QGrpcCallReply> m_currentReply;
+    // 保存底层的 gRPC 客户端指针
+    routeguide::RouteGuide::Client *m_grpcClient = nullptr; // 命名规则：包名::服务名::Client
 };
 
 #endif // GRPCCLIENT_H
